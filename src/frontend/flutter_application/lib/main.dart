ff70a3f3afcca68360f4887fa2fe1932c596f5b9
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-02-24 13:23:02 -0300

    Setup do Flutter e primeira página da aplicação finalizada

---
+++ src/frontend/flutter_application/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './widgets/home.dart';
+import './utils/materialColor.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GPT ROBOT',
+      theme: ThemeData(
+          primarySwatch: createMaterialColor(const Color(0xFF007D95)),
+          fontFamily: "Montserrat",
+          scaffoldBackgroundColor: Colors.black),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
