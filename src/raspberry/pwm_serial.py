4fa0632273e50b8e927206dff1df6b9b9043d7a6
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-03-27 17:33:21 -0300

    codigos raspberry pi comentados

--- src/raspberry/pwm_serial.py
+++ src/raspberry/pwm_serial.py
@@ -6,33 +6,45 @@ from time import sleep
 pwm = PWM(Pin(0))
 pwm.freq(1000)
 
+# Function to turn on the PWM so then the electromagnetic magnet will turn on
+# Max value for the PWM is 65_000
+
+
 def turn_on_PWM(value: int) -> bool:
-   try:
-      pwm.duty_u16(value)
-      return True
-   except:
-      return False
+    try:
+        pwm.duty_u16(value)
+        return True
+    except:
+        return False
+
+# Function to turn off(fixed value of 0) the PWM so then the electromagnetic magnet will turn on
+
 
 def turn_off_PWM() -> bool:
-   try:
-      pwm.duty_u16(0)
-      return True
-   except:
-      return False
+    try:
+        pwm.duty_u16(0)
+        return True
+    except:
+        return False
+
 
 # Loop of execution
 if __name__ == "__main__":
-   while True:
-      try:
-         # Realiza uma leitura de informação da serial
-         call = sys.stdin.readline().strip()
+    while True:
+        try:
+            # Will read any information that was passed throught the standar pin of the raspberry pi pico w.
+            call = sys.stdin.readline().strip()
 
-         if int(call) == 0:
-            turn_off_PWM()
+            # Cases that raspberry pi will perform some activity.
+            # If we pass 0, that means we want to turn off the PWM.
+            if int(call) == 0:
+                turn_off_PWM()
 
-         elif int(call) > 0 and int(call) <= 65000 :
-            turn_on_PWM(int(call))
-      
-      except:
-         sleep(0.4)
-         pass
+            # If we pass any value diferent from zero and less then the max value of PWM, it will turn on the electromagnetic magnet with that value.
+            elif int(call) > 0 and int(call) <= 65000:
+                turn_on_PWM(int(call))
+
+        except:
+            # Some activity so then the rapsberry wont watchdog.
+            sleep(0.4)
+            pass
--- src/raspberry/test/escritor_serial_pc.py
+++ src/raspberry/test/escritor_serial_pc.py
@@ -1,26 +1,34 @@
 import serial.tools.list_ports
 
+# Some values to config the serial communication
 tempo_espera = 2
 taxa_transmissao = 115200
 
-def find_coms():
+# Function to find the specific raspberry's port
 
+
+def find_coms():
+    # Get all ports available in your device
     ports_found = serial.tools.list_ports.comports()
-    print("[COM ports found]")
+    # Passing in each port to verify
     for port in ports_found:
+        # Will only return the port that is available
         if port.description != "n/a":
             print(port.device)
             return port.device
 
 
+# Storaging the port's name into the variable
 porta = find_coms()
-
-comunicacao_serial = serial.Serial(str(porta), taxa_transmissao, timeout = tempo_espera)
+# Setting up the serial communication
+comunicacao_serial = serial.Serial(
+    str(porta), taxa_transmissao, timeout=tempo_espera)
 
 
 while True:
+    # Ask for the user to input some value into the terminal and this value is storaged in the variable
     valor = str(input("Digite o valor que deseja passar para o raspberry: "))
-    comunicacao_serial.write(bytes(valor.encode()) + b"\n") # Escreve "on" na serial
+    # Send the value through the serial port configured in the variable "comunicacao_serial"
+    comunicacao_serial.write(bytes(valor.encode()) + b"\n")
+    # Some response for the user to confirm which value was sent
     print(f"Valor enviado! ---> {valor}")
-
-
