171c5faa7ae258468d403a17eefd180d0cf9f562
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-04-10 15:39:13 -0300

    Ajustes finais do projeto

--- src/backend/services/dobot.py
+++ src/backend/services/dobot.py
@@ -12,42 +12,77 @@ class Dobot:
         self._stage = 0
         self.pause = False
         self.sio = _sio
-        self.raspberry_instance = Raspberry()
-
+       
         self.tray = [
             [
                 {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
-                {"x": 10.1398, "y": -246.7926, "z": 102.9670, "r": -103.5851, "joint": True},
-                {"x": -63.5785, "y": -295.5353, "z": -22.8625, "r": -101.3776, "joint": False},
-                {"x": 91.9302, "y": -295.5699, "z": -22.8625, "r": -72.9041, "joint": False},
-                {"x": -79.5393, "y": -268.1194, "z": -22.8625, "r": -103.1598, "joint": False},
-                {"x": 96.3855, "y": -271.6446, "z": -34.8625, "r": -70.8550, "joint": False},
-                {"x": -79.0650, "y": -208.2662, "z": -22.8625, "r": -110.3335, "joint": False},
-                {"x": 99.2751, "y": -209.8143, "z": -22.8625, "r": -64.2235, "joint": False},
+                {"x": 0, "y": -235, "z": 151, "r": -89, "joint": True},
+                {"x": 93, "y": -282, "z": -10, "r": -71, "joint": False},
+                {"x": -61, "y": -288, "z": -10, "r": -101, "joint": False},
+                {"x": 88, "y": -258, "z": -10, "r": -70, "joint": False},
+                {"x": -60, "y": -253, "z": -10, "r": -103, "joint": False},
+                {"x": 88, "y": -229, "z": -10, "r": -68, "joint": False},
+                {"x": -63, "y": -236, "z": -10, "r": -104, "joint": False},
+                # {"x": 99.2751, "y": -209.8143, "z": -25, "r": -64.2235, "joint": False},
             ],
 
             [
                 {"x": 10.1398, "y": -246.7926, "z": 102.9670, "r": -103.5851, "joint": True},
                 {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
-                {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
-                {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
-                {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
-                {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
-                {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
-                {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": -47.6883, "z": -10, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": 80, "z": -10, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": -47.6883, "z": -10, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": 80, "z": -10, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": -47.6883, "z": -10, "r": -22.4403, "joint": True},
+                {"x": 200.7883, "y": 80, "z": -10, "r": -22.4403, "joint": True},
                 {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
                 {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
             ],
 
             [
                 {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
-                {"x": 64.0797, "y": 269.9920, "z": -20.7218, "r": 67.5687, "joint": True},
-                {"x": -70, "y": 269.9920, "z": -20.7218, "r": 67.5687, "joint": True},
-                {"x": 64.0797, "y": 269.9920, "z": -20.7218, "r": 67.5687, "joint": True},
-                {"x": -70, "y": 269.9920, "z": -20.7218, "r": 67.5687, "joint": True},
+                {"x": 64.0797, "y": 269.9920, "z": -10, "r": 67.5687, "joint": True},
+                {"x": -70, "y": 269.9920, "z": -10, "r": 67.5687, "joint": True},
+                {"x": 64.0797, "y": 269.9920, "z": -10, "r": 67.5687, "joint": True},
+                {"x": -70, "y": 269.9920, "z": -10, "r": 67.5687, "joint": True},
                 {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
             ]
         ]
+
+        # self.tray = [
+        #     [
+        #         {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
+        #         {"x": 10.1398, "y": -246.7926, "z": 102.9670, "r": -103.5851, "joint": True},
+        #         {"x": -63.5785, "y": -295.5353, "z": -25, "r": -101.3776, "joint": False},
+        #         {"x": 91.9302, "y": -295.5699, "z": -25, "r": -72.9041, "joint": False},
+        #         {"x": -79.5393, "y": -268.1194, "z": -25, "r": -103.1598, "joint": False},
+        #         {"x": 96.3855, "y": -271.6446, "z": -25, "r": -70.8550, "joint": False},
+        #         {"x": -79.0650, "y": -208.2662, "z": -25, "r": -110.3335, "joint": False},
+        #         {"x": 99.2751, "y": -209.8143, "z": -25, "r": -64.2235, "joint": False},
+        #     ],
+
+        #     [
+        #         {"x": 10.1398, "y": -246.7926, "z": 102.9670, "r": -103.5851, "joint": True},
+        #         {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
+        #         {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 200.7883, "y": -47.6883, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 200.7883, "y": 80, "z": -25.4011, "r": -22.4403, "joint": True},
+        #         {"x": 228, "y": 0, "z": 151, "r": 0, "joint": True},
+        #         {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
+        #     ],
+
+        #     [
+        #         {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
+        #         {"x": 64.0797, "y": 269.9920, "z": -30.7218, "r": 67.5687, "joint": True},
+        #         {"x": -70, "y": 269.9920, "z": -30.7218, "r": 67.5687, "joint": True},
+        #         {"x": 64.0797, "y": 269.9920, "z": -30.7218, "r": 67.5687, "joint": True},
+        #         {"x": -70, "y": 269.9920, "z": -30.7218, "r": 67.5687, "joint": True},
+        #         {"x": -7.2212, "y": 234.4742, "z": 123.2508, "r": 82.6842, "joint": True},
+        #     ]
+        # ]
 
     @property
     def cycle(self):
@@ -102,9 +137,11 @@ class Dobot:
             initial_stage = self.stage
 
             if self.stage == 2:
-                self.raspberry_instance.send_command("0")
+                raspberry_instance = Raspberry()
+                raspberry_instance.send_command("0")
             else:
-                self.raspberry_instance.send_command(str(self.magneticForce))
+                raspberry_instance = Raspberry()
+                raspberry_instance.send_command(str(self.magneticForce))
 
             for cords in self.tray[self.stage]:
                 if self.stage != initial_stage:
@@ -128,11 +165,18 @@ class Dobot:
                                         cords['z'],
                                         cords['r'],
                                         wait=True)
+                
+                
                 self.sio.sleep(0)
 
             if (self.stage == 2):
                 self.cycle += 1
 
+            self.sio.sleep(0.5)
+            if self.stage != initial_stage:
+                (x, y, x, r, j1, j2, j3, j4) = self.device.pose()
+                self.change_tray({'x': x, 'y':  y, 'z': 151, 'r': r})
+                raise NameError("Stage changed!")
             self.stage += 1
         except NameError as err:
             print(err)
@@ -143,6 +187,9 @@ class Dobot:
             self.device = 0
             self.cycle = 0
             self.stage = 0
+
+            raspberry_instance = Raspberry()
+            raspberry_instance.send_command("0")
 
             self.sio.emit("response_emergency_stop",
                           "Emergency stop with success!")
